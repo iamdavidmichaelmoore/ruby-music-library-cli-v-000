30dc39f7c0532f9d6acfc7882967621333bdf2a0
David Moore <hey@iamdavidmoore.com>
2018-06-16 21:07:19 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,6 +42,7 @@ class MusicLibraryController
   def list_artists
     sorted_artists = Artist.all.sort_by {|artist| artist.name}.uniq
     sorted_artists.each_with_index(1) do |artist, index|
+      binding.prys
       puts "#{index} #{artist.name}"
       binding.pry
     end
